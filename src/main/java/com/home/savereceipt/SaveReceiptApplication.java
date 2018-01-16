0a7d168028de74a446c5ef01f8fe9ca6424a8007
Dariusz <rozik85@o2.pl>
2018-01-16 21:58:28 +0100

    first commit, connection to the database

---
+++ src/main/java/com/home/savereceipt/SaveReceiptApplication.java
@@ -0,0 +1,12 @@
+package com.home.savereceipt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaveReceiptApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaveReceiptApplication.class, args);
+	}
+}
